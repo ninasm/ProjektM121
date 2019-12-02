2d3dd0936c905f36896f4e47ee4421021b5ff642
Gerfatz <46902765+Gerfatz@users.noreply.github.com>
2019-12-02 11:30:34 +0100

    initial commit

---
+++ LiftSimulation/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file LiftSimulation.c
+ * @brief Hauptprogramm und entry point der Liftsteuerung
+ *
+ * @date 20.11.2019 Erstellen des Templates
+ * @author
+ */ 
+
+#include "LiftLibrary.h"
+#include "LiftSimulationCommon.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+   InitializePorts();
+}
+
