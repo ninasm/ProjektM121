ea9cc7139cbd76908b5375fbba17eabb884ad602
ninasm <44799867+ninasm@users.noreply.github.com>
2019-12-09 12:42:23 +0100

    Update MotorController.c

--- LiftSimulation/MotorController.c
+++ LiftSimulation/MotorController.c
@@ -7,3 +7,59 @@
  */ 
 
 #include "LiftSimulationCommon.h"
+
+MotorController _motorCtrl =
+{
+	.start = Floor2,
+	.target = Floor0,
+	.fsm  = { .Next = 0, .CurrentState = MotorCtrl_Initializing, .RxMask = 0xFF },
+};
+
+void NotifyCalibrationDone(uint8_t currentPos, uint8_t targetPostion)
+{
+	FloorType floor = (FloorType)currentPos/16;
+	SetDisplay(floor);
+	if( ((currentPos %floor) == 0 ) && floor == Floor0 )
+	{
+		SendEvent(SignalSourceEnvironment, LiftCalibrated, currentPos, targetPostion);
+	}
+}
+
+void MotorCtrl_Initializing(Message* msg)
+{
+	if( msg->Id == LiftStarted)
+	{
+		CalibrateElevatorPosition(NotifyCalibrationDone);
+		return;
+	}
+	if( msg->Id == LiftCalibrated )
+	{
+		SetDisplay(Floor0);
+		SetState(&_motorCtrl.fsm, MotorCtrl_Stopped);
+	}
+}
+
+void OnElevatorPositionChanged(uint8_t currentPos, uint8_t targetPos)
+{
+	SendEvent(SignalSourceElevator, Message_PosChanged, currentPos, targetPos);
+}
+
+void MotorCtrl_Stopped(Message* msg)
+{
+	if( msg->Id == Message_MoveTo && msg->MsgParamLow < 4)
+	{
+		_motorCtrl.target = (FloorType)msg->MsgParamLow;
+		SetState(&_motorCtrl.fsm, MotorCtrl_Moving);
+		MoveElevator(_motorCtrl.target * POS_STEPS_PER_FLOOR, OnElevatorPositionChanged );
+	}
+}
+
+void MotorCtrl_Moving(Message* msg)
+{
+	if( msg->Id == Message_PosChanged && msg->MsgParamLow == msg->MsgParamHigh)
+	{
+		_motorCtrl.target = (FloorType)msg->MsgParamLow;
+		SetState(&_motorCtrl.fsm, MotorCtrl_Stopped);
+		SetDoorState(DoorOpen, msg->MsgParamLow/POS_STEPS_PER_FLOOR);
+	}
+}
